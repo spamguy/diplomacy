35ea55cbb568b649560030b672cb4bbe1ea1c133
Will Oram <spamguy@spamguy.org>
2015-01-25 09:08:37 -0600

    Readding karma config

---
+++ karma.conf.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = function (config) {
+	config.set({
+		frameworks: ['jasmine'],
+        files: [
+          //'node_modules/jquery/dist/jquery.js',
+          'node_modules/lodash/dist/lodash.js',
+	      'node_modules/angular/angular.js',
+	      'node_modules/angular-mocks/angular-mocks.js',
+	      'node_modules/angular-ui-router/release/angular-ui-router.js',
+	      'node_modules/angular-bootstrap/ui-bootstrap.js',
+	      'node_modules/angular-ui-form-validation/dist/angular-ui-form-validation.js',
+	      'node_modules/angular-animate/angular-animate.js',
+	      'node_modules/angular-local-storage/dist/angular-local-storage.js',
+	      'node_modules/angular-jwt/dist/angular-jwt.js',
+	      'node_modules/restangular/dist/restangular.js',
+
+	      // client files
+	      'client/app/app.module.js',
+	      'client/app/app.controller.js',
+	      'client/app/**/*.service.js',
+	      'client/app/**/*.directive.js',
+	      'client/app/**/*.module.js',
+	      'client/app/**/*.state.js',
+          'client/app/**/*.filter.js',
+	      'client/app/**/*.controller.js',
+	      'client/app/**/*.html',
+	      'client/app/**/*.spec.js'
+        ],
+        logLevel:'ERROR',
+        reporters:['mocha'],
+        autoWatch: false,
+        singleRun: true,
+        browsers: ['PhantomJS'],
+        preprocessors: {
+        	'app/**/*.html': 'html2js'
+        },
+        ngHtml2JsPreprocessor: {
+		    // strip app from the file path
+		    stripPrefix: 'app/'
+		}
+	});
+};
