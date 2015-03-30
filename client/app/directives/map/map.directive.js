998c6514ccafca65d73e4cc70c639d9a3fab8fbe
Will Oram <spamguy@spamguy.org>
2015-03-29 21:46:59 -0500

    Added failed command support. Fixes #3.

--- client/app/directives/map/map.directive.js
+++ client/app/directives/map/map.directive.js
@@ -4,7 +4,7 @@ angular.module('map.directives', ['SVGService'])
 
     var regionDictionary = {};
 
-    var absURL = "";
+    var absURL = '';
 
     var regionClicked = function() {
         console.log(this.id);
@@ -12,21 +12,18 @@ angular.module('map.directives', ['SVGService'])
         // TODO: Order input logic
     };
 
-    var getCentroid = function(selection) {
-        var bbox = selection.getBBox();
-        // return the center of the bounding box
-        return [bbox.x + bbox.width/2, bbox.y + bbox.height/2];
-    };
-
     var generateCurvedArrow = function(d) {
         var dx = regionDictionary[d.target.r].x - regionDictionary[d.source.r].x,
             dy = regionDictionary[d.target.r].y - regionDictionary[d.source.r].y,
             dr = Math.sqrt(dx * dx + dy * dy);
 
-        return "M" + regionDictionary[d.source.r].x + "," + regionDictionary[d.source.r].y + "A" + dr + "," + dr + " 0 0,1 " + regionDictionary[d.target.r].x + "," + regionDictionary[d.target.r].y;
+        return 'M' + regionDictionary[d.source.r].x + ',' + regionDictionary[d.source.r].y + 'A' + dr + ',' + dr + ' 0 0,1 ' + regionDictionary[d.target.r].x + ',' + regionDictionary[d.target.r].y;
     };
 
-    var generateMarkerEnd = function(d) { return 'url(' + absURL + '#' + d.target.action + ')'; };
+    var generateMarkerEnd = function(d) {
+        // see CSS file for why separate markers exist for failed orders
+        var failed = d.target.failed ? 'failed' : '';
+        return 'url(' + absURL + '#' + failed + d.target.action + ')'; };
 
     var generateSVG = function(variant, season, readonly, el) {
         if (!variant || !season)
@@ -42,23 +39,26 @@ angular.module('map.directives', ['SVGService'])
             // STEP 1: build root <svg> -------------------
             var svg = d3.select(el)
                 .append('svg')
-                .attr("viewBox", '0 0 ' + xml.rootElement.getAttribute('width') + ' ' + xml.rootElement.getAttribute('height'));
+                .attr('viewBox', '0 0 ' + xml.rootElement.getAttribute('width') + ' ' + xml.rootElement.getAttribute('height'));
             // --------------------------------------------
 
             // STEP 2: build templated items --------------
-            var defs = svg.append("svg:defs");
+            var defs = svg.append('svg:defs');
 
             // generic curved arrow
-            defs.selectAll("marker")
-                .data(['move', 'support'])      // mapping movement types to CSS classes
-                .enter().append("svg:marker")
-                .attr("id", String)
-                .attr("viewBox", "0 -5 10 10")
-                .attr("markerWidth", 6)
-                .attr("markerHeight", 6)
-                .attr("orient", "auto")
-                .append("svg:path")
-                .attr("d", "M0,-5L10,0L0,5");
+            defs.selectAll('marker')
+                .data(['move', 'support', 'failedmove', 'failedsupport'])      // mapping movement types to CSS classes
+                .enter().append('svg:marker')
+                .attr('id', String)
+                .attr('viewBox', '0 -5 10 10')
+                .attr('markerWidth', 6)
+                .attr('markerHeight', 6)
+                .attr('orient', 'auto')
+                .attr('class', function(d) {
+                    return _.startsWith(d, 'failed') ? 'failed' : 'ok';
+                })
+                .append('svg:path')
+                .attr('d', 'M0,-5L10,0L0,5');
 
             // supply centre star
             SVGService.getStar(function(star) {
@@ -89,7 +89,7 @@ angular.module('map.directives', ['SVGService'])
             var scGroup = svg.append('g')
                 .attr('id', 'scGroup')
                 .selectAll('path')
-                .data(_.filter(season.regions, function(r) { return r.sc; }))
+                .data(_.filter(season.regions, function(r) { return !_.isUndefined(r.sc); }))
                 .enter();
 
             // append one pretty coloured star per SC
@@ -99,13 +99,14 @@ angular.module('map.directives', ['SVGService'])
                 .attr('transform', function(d) {
                     return 'translate(' + regionDictionary[d.r].sc.x + ',' + regionDictionary[d.r].sc.y + ') scale(0.03)'; })
                 .attr('fill', function(d) {
-                    return d.sc && d.sc.ownedBy ? variant.powers[d.sc.ownedBy].colour : '#bbbbbb';
+                    return d.sc ? variant.powers[d.sc].colour : '#bbbbbb';
                 });
             // --------------------------------------------
 
             // STEP 6: apply unit marker layer ------------
             var unitGroup = svg.append('g')
                 .attr('id', 'unitGroup');
+
             unitGroup
                 .selectAll('circle')
                 .data(_.filter(season.regions, function(r) { return r.unit && r.unit.type === 1; }))
@@ -119,13 +120,20 @@ angular.module('map.directives', ['SVGService'])
                 .attr('fill', function(d) {
                     return variant.powers[d.unit.power].colour;
                 });
+
             unitGroup
                 .selectAll('rect')
-                .data(_.filter(season.regions, function(r) { return r.unit && r.unit.type === 2; }))
+                .data(_.filter(season.regions, function(r) {
+                    return r.unit && r.unit.type === 2;
+                }))
                 .enter()
                 .append('rect')
-                .attr('x', function(d) { return regionDictionary[d.r].x - 10; })
-                .attr('y', function(d) { return regionDictionary[d.r].y - 5; })
+                .attr('x', function(d) {
+                    return regionDictionary[d.r].x - 10;
+                })
+                .attr('y', function(d) {
+                    return regionDictionary[d.r].y - 5;
+                })
                 .attr('height', 10)
                 .attr('width', 20)
                 .attr('stroke-width', '1px')
@@ -143,7 +151,11 @@ angular.module('map.directives', ['SVGService'])
                     var target = region.unit.order.y1 || region.unit.order.y2;
                     links.push({
                         source: _.defaults(region, { fixed: true }),
-                        target: _.defaults(regionDictionary[target], { fixed: true, action: region.unit.order.action })
+                        target: _.defaults(regionDictionary[target], {
+                            fixed: true, // to keep d3 from treating this map like a true force graph
+                            action: region.unit.order.action,
+                            failed: region.unit.order.failed
+                        })
                     });
                 }
             }
@@ -164,10 +176,13 @@ angular.module('map.directives', ['SVGService'])
                 force.stop();
 
                 moveGroup
-                    .append("svg:path")
-                    .attr("marker-end", generateMarkerEnd)
-                    .attr('class', 'link move')
-                    .attr("d", generateCurvedArrow);
+                    .append('svg:path')
+                    .attr('marker-end', generateMarkerEnd)
+                    .attr('class', function(d) {
+                        var failed = d.target.failed ? 'failed ' : 'ok ';
+                        return failed + 'link move';
+                    })
+                    .attr('d', generateCurvedArrow);
             }
         });
     };
@@ -177,8 +192,8 @@ angular.module('map.directives', ['SVGService'])
         scope: {
             variantPromise: '=variant',        // full variant data (JSON)
             seasonPromise: '=season',          // promise resolving to movement data (JSON)
-            readonly: '=readonly',      // whether to allow user interaction (bool)
-            arrows: '=arrows'           // whether to show movement arrows -- true implies season is defined (bool)
+            readonly: '=readonly',             // whether to allow user interaction (bool)
+            arrows: '=arrows'                  // whether to show movement arrows -- true implies season is defined (bool)
         },
         restrict: 'E',
         link: function(scope, element, attrs) {
