f20208d008741c523ae5131adfc6b96896df1e91
Will Oram <spamguy@spamguy.org>
2015-05-05 21:51:02 -0500

    * Fixed game list item appearance * Added filtering to game list

---
+++ client/app/games/view/filters/variant.filter.spec.js
@@ -0,0 +1,29 @@
+describe('Game list variant filter', function () {
+    'use strict';
+
+    var filter;
+
+    beforeEach(function() {
+        module('games');
+
+        inject(function($filter) {
+            filter = $filter('variant');
+        });
+    });
+
+    it('matches against variant names', function() {
+        expect(
+            filter([
+                { variant: 'Standard' }, { variant: 'Standard' }, { variant: 'Chromatic' }
+            ], 'Standard').length)
+        .toEqual(2);
+    });
+
+    it('is case-insensitive', function() {
+        expect(
+            filter([
+                { variant: 'Standard' }, { variant: 'standard' }, { variant: 'stANDard' }
+            ], 'STANdard').length)
+        .toEqual(3);
+    });
+});
