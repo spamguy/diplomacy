f764427448863d643523772140aad3a11618d800
Will Oram <spamguy@spamguy.org>
2015-07-25 22:45:35 -0500

    Checkpoint

--- client/app/main/signup/signup.controller.js
+++ client/app/main/signup/signup.controller.js
@@ -1,33 +1,20 @@
 'use strict';
 
 angular.module('diplomacy.main')
-    .controller('SignupController', ['$scope', '$http', 'loginService', function ($scope, $http, loginService) {
-        // TODO: force logged-in users out of this page
+.controller('SignupController', ['$scope', '$http', 'loginService', function ($scope, $http, loginService) {
+    // TODO: force logged-in users out of this page
 
-        angular.extend($scope, {
-            user: {
-                username: null,
-                password: null,
-                password2: null,
-                email: null,
-                points: 0,
-                timezone: 0,
-                save: function() {
-                    $http.post('/api/users', this).then(loginService.validLoginCallback);
-                }
+    angular.extend($scope, {
+        user: {
+            username: null,
+            password: null,
+            password2: null,
+            email: null,
+            points: 0,
+            timezone: 0,
+            save: function() {
+                //$http.post('/api/users', this).then(loginService.validLoginCallback);
             }
-        });
-
-        $scope.canExitUsernameStep = function() {
-            return $scope.signupForm.username.$valid;
-        };
-
-        $scope.canExitPasswordStep = function() {
-            return $scope.signupForm.password.$valid &&
-                $scope.signupForm.password2.$valid;
-        };
-
-        $scope.onWizardFinished = function() {
-            $scope.user.save();
-        };
-    }]);
+        }
+    });
+}]);
