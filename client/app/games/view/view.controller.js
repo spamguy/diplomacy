5958d7a09f17913ea8b5a4a7c611f5441893a7e7
Will Oram <spamguy@spamguy.org>
2015-08-11 22:05:18 -0500

    Added admin panel to game view

--- client/app/games/view/view.controller.js
+++ client/app/games/view/view.controller.js
@@ -1,12 +1,23 @@
 'use strict';
 
 angular.module('games')
-.controller('ViewController', ['$scope', 'gameService', 'game', function ($scope, gameService, game) {
+.controller('ViewController', ['$scope', 'gameService', 'userService', 'game', function ($scope, gameService, userService, game) {
     var variant = gameService.getVariant(game.variant);
 
     $scope.variant = variant;
 
-    if (game.isAdmin)
+    // identify whether current player is admin of this game
+    var playerID = userService.getCurrentUser(),
+        isAdmin = false;
+    for (var p = 0; p < game.players.length; p++) {
+        if (game.players[p].power === '*' && game.players[p].player_id === playerID) {
+            isAdmin = true;
+            break;
+        }
+    }
+    $scope.isAdmin = isAdmin;
+
+    if (isAdmin)
         $scope.season = gameService.getMoveData(game._id);
     else
         $scope.season = gameService.getMoveDataForCurrentUser(game._id);
