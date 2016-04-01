1c9e3aa0f0bf2c83a7372e8f2da01eed872b003a
Will Oram <spamguy@spamguy.org>
2016-04-01 14:01:47 -0500

    Checkpoint: mostly adapted old d3 force graph code

--- client/app/components/map/map.controller.js
+++ client/app/components/map/map.controller.js
@@ -3,7 +3,18 @@ angular.module('map.component')
     var vm = this,
         paths = vm.svg.getElementsByTagName('path'),
         regionReferenceDictionary = _.indexBy(this.variant.regions, 'r'),
-        p;
+        p,
+        i,
+        region,
+        unitInRegion,
+        target,
+        links = [],
+        force,
+        moveGroup = d3.select('svg g.moveLayer'),
+        evenMorePadding,
+        unitRadiusPlusPadding,
+        pathLength,
+        midpoint;
 
     vm.getSVGAttribute = getSVGAttribute;
     vm.isHeaderVisible = isHeaderVisible;
@@ -36,6 +47,68 @@ angular.module('map.component')
         vm.commandData = [];
         vm.currentAction = 'hold';
         vm.clickCount = 0;
+
+        // Build force directed graph.
+        for (p = 0; p < this.season.regions.length; p++) {
+            region = this.season.regions[p];
+            unitInRegion = gameService.getUnitOwnerInRegion(region);
+            if (unitInRegion && unitInRegion.unit.order)
+                target = unitInRegion.unit.order.y1 || unitInRegion.unit.order.y2 || unitInRegion.r;
+            else
+                continue;
+
+            links.push({
+                source: _.defaults(region, { fixed: true }),
+                target: _.defaults(regionReferenceDictionary[target], {
+                    fixed: true, // To keep d3 from treating this map like a true force graph.
+                    action: unitInRegion.unit.order.action,
+                    failed: unitInRegion.unit.order.failed
+                })
+            });
+        }
+
+        if (!links.length)
+            return;
+
+        force = d3.layout.force()
+            .nodes(regionReferenceDictionary)
+            .links(links)
+            .on('tick', onForceDirectedGraphTick.bind(this)); // bind() forces function's scope to controller.
+        moveGroup = d3.select('svg g.moveLayer')
+            .selectAll('path')
+            .data(force.links())
+            .enter()
+            .append('svg:path')
+            .attr('marker-end', generateMarkerEnd)
+            .attr('class', function(d) {
+                var failed = d.target.failed ? 'failed ' : 'ok ';
+                return failed + 'link move';
+            })
+            .attr('id', function(d) { return d.source.r + '-link'; });
+
+        // Append circles to units perceived to or actually holding.
+        moveGroup
+            .select('circle')
+            .data(_.filter(this.season.regions, function(r) {
+                if (r.unit && r.unit.order) {
+                    if (r.unit.order.action === 'hold')
+                        return true;
+                    else if (r.unit.order.action === 'support' && !r.unit.order.y2)
+                        return true;
+                    else
+                        return false;
+                }
+            }))
+            .enter()
+            .append('circle')
+            .attr('class', 'hold')
+            .attr('cx', function(d) { return regionReferenceDictionary[d.r].x; })
+            .attr('cy', function(d) { return regionReferenceDictionary[d.r].y; })
+            .attr('r', unitRadiusPlusPadding);
+
+        force.start();
+        for (i = 20; i > 0; --i) force.tick();
+        force.stop();
     }
 
     function getSVGAttribute(attr) {
@@ -139,5 +212,76 @@ angular.module('map.component')
             if (y2)
                 unitInRegion.order.y2 = y2;
         }
+    }
+
+    function generateMarkerEnd(d) {
+        // See CSS file for why separate markers exist for failed orders.
+        var failed = d.target.failed ? 'failed' : '';
+        return 'url(' + vm.scPath + '#' + failed + d.target.action + ')';
+    }
+
+    function onForceDirectedGraphTick(e, scope) {
+        var regions = this.season.regions,
+            svg = this.svg;
+        moveGroup.attr('d', function(d) {
+            /*
+             * Let T -> target, T' -> target of target, and S -> source.
+             *
+             * The endpoint of this path depends on a) what S intends to do, and b) what T intends to do.
+             * If S intends to complement T, and if T' exists, the endpoint should exist somewhere on the T - T' path to indicate the support.
+             * In all other cases T as an endpoint is fine.
+             */
+
+            var sx = regionReferenceDictionary[d.source.r].x,
+                sy = regionReferenceDictionary[d.source.r].y,
+                tx = regionReferenceDictionary[d.target.r].x,
+                ty = regionReferenceDictionary[d.target.r].y,
+                dx,
+                dy,
+                action = d.target.action,
+                actionOfTarget,
+                pathOfTarget,
+                dr,
+                targetUnit;
+
+            // If a hold, don't draw any arrows.
+            if (d.target.r === d.source.r)
+                return;
+
+            targetUnit = _.find(regions, 'r', d.target.r);
+            if (targetUnit.unit && targetUnit.unit.order)
+                actionOfTarget = targetUnit.unit.order.action;
+
+            if (action === 'move')
+                evenMorePadding = 5;
+            else
+                evenMorePadding = 0;
+
+            // Figure out a good corner to which to point.
+            if (sx > tx)
+                tx += unitRadiusPlusPadding + evenMorePadding;
+            else
+                tx -= unitRadiusPlusPadding + evenMorePadding;
+
+            if (sy > ty)
+                ty -= unitRadiusPlusPadding + evenMorePadding;
+            else
+                ty += unitRadiusPlusPadding + evenMorePadding;
+
+            dx = tx - sx;
+            dy = ty - sy;
+
+            if (action !== 'support' || actionOfTarget !== 'move') {
+                dr = Math.sqrt(dx * dx + dy * dy);
+                return 'M' + sx + ',' + sy + 'A' + dr + ',' + dr + ' 0 0,1 ' + tx + ',' + ty;
+            }
+            else if (actionOfTarget === 'move') {
+                pathOfTarget = svg.select('path#' + d.target.r)[0][0];
+                pathLength = pathOfTarget.getTotalLength();
+                midpoint = pathOfTarget.getPointAtLength(pathLength / 2);
+
+                return 'M' + sx + ',' + sy + 'L' + midpoint.x + ',' + midpoint.y;
+            }
+        });
     }
 }]);
